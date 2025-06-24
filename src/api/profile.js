bfcf2043316f83bea5b5b37c952bb0d50be4a8f6
Yeong-si <dtd1110@gmail.com>
2025-06-24 15:34:52 +0900

    FEAT : 대학생 프로필 리스트 조회 API 연동

---
+++ src/api/profile.js
@@ -0,0 +1,17 @@
+import client from "./client";
+
+export const getProfile = async (firstCategory, pageable) => {
+  try {
+    const response = await client.get("/api/v1/member", {
+      params: {
+        firstCategory,
+        page: pageable.page,
+        size: pageable.size,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("인기 피드 조회 에러:", error);
+    throw error;
+  }
+};
