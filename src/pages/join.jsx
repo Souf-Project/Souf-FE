058c9fdc71559a7586b0b5564d0c8bf85775ad6b
Yeong-si <dtd1110@gmail.com>
2025-07-16 02:23:16 +0900

    DESIGN : 반응형 웹 join

--- src/pages/join.jsx
+++ src/pages/join.jsx
@@ -10,9 +10,9 @@ export default function Join({}) {
   const navigate = useNavigate();
   //const [step, setStep] = useState(1);
   return (
-    <div className="flex items-center justify-center my-20 ">
-      <div className="w-[1000px]">
-        <div className="font-semibold text-[60px]">회원 가입</div>
+    <div className="flex items-center justify-center my-20 w-full">
+      <div className="w-full max-w-[1000px] px-4">
+        <div className="font-semibold text-3xl sm:text-[48px] md:text-[60px] max-sm:pl-4">회원 가입</div>
         <Step1 />
       </div>
     </div>
