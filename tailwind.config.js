5c6b28c20c4731bbac40ecc5b893822e78b61f1b
tldms0507 <sieun050783@gmail.com>
2025-09-22 17:04:56 +0900

    DESIGN : 타이틀 애니메이션 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,12 +45,12 @@ module.exports = {
       },
       keyframes: {
         'slide-up': {
-          '0%': { transform: 'translateY(0)', opacity: '1' },
-          '100%': { transform: 'translateY(-50%)', opacity: '0' },
+          '0%': { transform: 'translateX(0)', opacity: '1' },
+          '100%': { transform: 'translateX(2%)', opacity: '0' },
         },
         'slide-down': {
-          '0%': { transform: 'translateY(100%)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
+          '0%': { transform: 'translateX(-5%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
         },
       },
     },
