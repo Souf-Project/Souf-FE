d766c28530bae486c06046ae39b215dce9ed9fc4
tldms0507 <sieun050783@gmail.com>
2025-10-01 18:02:37 +0900

    DESIGN : 회원 탈퇴 페이지 메인 컬러 변경

--- src/components/withdraw/withdrawDescription.jsx
+++ src/components/withdraw/withdrawDescription.jsx
@@ -9,7 +9,7 @@ export default function WithdrawDescription({ onWithdraw }) {
   return (
     <>
       <h1 className="text-3xl font-bold mb-2 text-gray-800">지금 떠나시면 ...</h1>
-      <h1 className="text-3xl font-bold mb-6 text-[#FFC105]">데이터가 영영 사라져요 🥹</h1>
+      <h1 className="text-3xl font-bold mb-6 text-blue-main">데이터가 영영 사라져요 🥹</h1>
       <p className="mb-6 text-gray-600">
         탈퇴 시 모든 데이터는 복구할 수 없습니다.<br/>
         또한, 탈퇴 시 해당 이메일은 7일간 재가입이 불가능합니다.<br/>
@@ -18,13 +18,13 @@ export default function WithdrawDescription({ onWithdraw }) {
       <div className="flex justify-center gap-4">
         <button
           onClick={() => navigate(-1)}
-          className="px-6 py-3 rounded-md bg-gray-200 font-bold hover:bg-gray-300"
+          className="px-6 py-3 rounded-md bg-gray-200 font-bold hover:shadow-md"
         >
           취소
         </button>
         <button
           onClick={onWithdraw}
-          className="px-6 py-3 rounded-md bg-[#FFE58F] font-bold hover:bg-[#FFC105]"
+          className="px-6 py-3 rounded-md bg-blue-main font-bold hover:shadow-md text-white"
         >
           탈퇴하기
         </button>
