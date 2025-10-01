3cce3de2201ccc2bad8ed38d4b70298999883941
tldms0507 <sieun050783@gmail.com>
2025-10-01 19:27:12 +0900

    FEAT : 본인 게시물에 대한 수정 및 삭제 버튼 위치 조정

--- src/components/studentProfile/postDetail.jsx
+++ src/components/studentProfile/postDetail.jsx
@@ -279,6 +279,39 @@ const handleDeleteClick = () => {
                     <p className="text-sm text-gray-500 whitespace-nowrap">
             조회 수 {worksData.view}회
           </p>
+          {/* 수정 버튼 (오른쪽) - 본인일 경우에만 */}
+{Number(id) === memberId && (
+                      <div ref={optionsRef} className="relative">
+                        <button
+                          onClick={() => setShowOptions((prev) => !prev)}
+                          className="text-xl px-2 py-1 rounded hover:bg-gray-100"
+                        >
+                          ⋯
+                        </button>
+
+                        {showOptions && (
+                          <div className="absolute right-0 top-full mt-1 w-28 bg-white border rounded shadow-lg z-10">
+                            <button
+                              onClick={() => navigate("/postEdit", {
+                                        state: {
+                                        worksData,
+                                        mediaData
+                                      }
+                                    })}
+                              className="w-full text-left px-4 py-2 hover:bg-gray-100 text-sm"
+                            >
+                              수정하기
+                            </button>
+                            <button
+                               onClick={handleDeleteClick}
+                              className="w-full text-left px-4 py-2 hover:bg-gray-100 text-sm text-red-500"
+                            >
+                              삭제하기
+                            </button>
+                          </div>
+                        )}
+                      </div>
+                    )}
             </div>
          
             <div className="flex  w-full h-full relative">
@@ -342,56 +375,13 @@ const handleDeleteClick = () => {
             <div className="w-full relative order-1 lg:order-2 mb-6 lg:mb-0">
               <div className="flex flex-col justify-between h-full">
                 {/* 상단: 프로필 + 제목 + 내용 */}
-                <div>
-                  <div className="flex items-center justify-between mb-4 w-full">
-                   
-                    
-                    
-                    {/* 수정 버튼 (오른쪽) - 본인일 경우에만 */}
-                    {Number(id) === memberId && (
-                      <div ref={optionsRef}>
-                        <button
-                          onClick={() => setShowOptions((prev) => !prev)}
-                          className="text-xl px-2 py-1 rounded hover:bg-gray-100"
-                        >
-                          ⋯
-                        </button>
-
-                        {showOptions && (
-                          <div className="absolute right-0 mt-2 w-28 bg-white border rounded shadow-lg z-10">
-                            <button
-                              onClick={() => navigate("/postEdit", {
-                                        state: {
-                                        worksData,
-                                        mediaData
-                                      }
-                                    })}
-                              className="w-full text-left px-4 py-2 hover:bg-gray-100 text-sm"
-                            >
-                              수정하기
-                            </button>
-                            <button
-                               onClick={handleDeleteClick}
-                              className="w-full text-left px-4 py-2 hover:bg-gray-100 text-sm text-red-500"
-                            >
-                              삭제하기
-                            </button>
-                          </div>
-                        )}
-                      </div>
-                    )}
-                  </div>
-
-
-                  
-                    <div className="w-full">
+                 
+                    <div className="w-full mt-6">
                       <p className="text-xs font-semibold mb-2">작품 소개</p>
                       <p className="whitespace-pre-wrap text-sm font-medium leading-relaxed break-words overflow-hidden">
                         {worksData.content}
                       </p>
                       </div>
-
-                </div>
 
                 <div className="flex items-center justify-between gap-2 mt-4">
                   <div className="flex items-center gap-2">
