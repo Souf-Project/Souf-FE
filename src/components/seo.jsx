bc576e1f3acc13460d06abe8decdd0b5e5dbe61b
Yeong-si <dtd1110@gmail.com>
2025-11-30 15:16:49 +0900

    FIX : SEO 내부 공모전 텍스트 삭제

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -7,7 +7,7 @@ import { Helmet } from "react-helmet-async";
 export default function SEO({ title, subTitle ="", description, content }) {
   // title과 subTitle이 유효한 문자열인지 확인
   const safeTitle = title || "SouF";
-  const safeSubTitle = subTitle || "대학생 외주 & 공모전";
+  const safeSubTitle = subTitle || "대학생 외주";
 
     useEffect(() => {
     document.title = `${safeTitle} | ${safeSubTitle}`;
@@ -17,9 +17,9 @@ export default function SEO({ title, subTitle ="", description, content }) {
     <Helmet>
       <title>{`${safeTitle} | ${safeSubTitle}`}</title>
       <meta property="title" content={`${safeTitle} | SouF`} />
-      <meta property="description" content={content || description || "스프 SouF 대학생 외주 공모전"} />
+      <meta property="description" content={content || description || "스프 SouF 대학생 외주"} />
       <meta property="og:title" content={`${safeTitle} | SouF`} />
-      <meta property="og:description" content={content || description || "스프 SouF 대학생 외주 공모전"} />
+      <meta property="og:description" content={content || description || "스프 SouF 대학생 외주"} />
       <meta charSet="utf-8" />
     </Helmet>
   );
