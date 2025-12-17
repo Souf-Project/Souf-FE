8ca88b7902a1a3dee30ebad8070b030fceaa9aab
tldms0507 <sieun050783@gmail.com>
2025-12-17 18:41:21 +0900

    DESIGN : 고객센터 - FAQ, 문의센터 모바일 반응형 작업

--- src/components/cs/accordian.jsx
+++ src/components/cs/accordian.jsx
@@ -14,7 +14,7 @@ export default function Accordian({ question, answer, ulList, olList, quotation 
         onClick={toggleAccordion}
         className="w-full py-4 text-left bg-white transition-colors duration-200 flex items-center justify-between"
       >
-        <p className="text-blue-500 text-xl font-semibold">Q. {question}</p>
+        <p className="text-blue-500 text-md md:text-xl font-semibold">Q. {question}</p>
         <img
           src={arrow}
           alt="화살표"
@@ -32,23 +32,23 @@ export default function Accordian({ question, answer, ulList, olList, quotation 
         <div className="h-[1px] bg-gray-200 my-2"></div>
         <div className="py-4">
        
-          <p className="text-base font-medium text-black">A. {answer}</p>
+          <p className="text-sm md:text-base font-medium text-black">A. {answer}</p>
           {ulList ? (
             <ul className="list-disc list-inside space-y-2 mt-4">
               {ulList.map((item, index) => (
-                <li key={index} className="text-gray-700 font-medium">{item}</li>
+                <li key={index} className="text-sm md:text-base text-gray-700 font-medium">{item}</li>
               ))}
             </ul>
           ) : null}
           {olList ? (
             <ol className="list-decimal list-inside space-y-2 mt-4">
               {olList.map((item, index) => (
-                <li key={index} className="text-gray-700 font-medium">{item}</li>
+                <li key={index} className="text-sm md:text-base text-gray-700 font-medium">{item}</li>
               ))}
             </ol>
           ) : null}
           {quotation ? (
-            <p className="text-base font-light text-gray-700 mt-4 border-l-2 border-gray-300 pl-4 py-2">{quotation}</p>
+            <p className="text-sm md:text-base font-light text-gray-700 mt-4 border-l-2 border-gray-300 pl-4 py-2">{quotation}</p>
           ) : null}
         </div>
       </div>
