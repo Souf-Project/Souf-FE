9060ca437a0c53454175d74e651d870738d5d086
tldms0507 <sieun050783@gmail.com>
2026-02-20 18:48:59 +0900

    FEAT : 기업 미승인 유저 에러키 추가

--- src/constants/recruit.js
+++ src/constants/recruit.js
@@ -1,4 +1,8 @@
 export const RECRUIT_ERRORS = {
+  "M401-1": {
+    message: "승인되지 않은 계정입니다\n기업 승인 후 업로드 가능합니다.",
+    action: null
+  },
   "R400-4": {
     message: "견적 금액은 공백일 수 없습니다.",
     action: null, 
